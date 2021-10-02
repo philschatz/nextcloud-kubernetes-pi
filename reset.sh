6d4eae1eae7753e848ba746829fa54417783f67d
philschatz <253202+philschatz@users.noreply.github.com>
2021-10-01 22:44:23 -0500

    add photoprism

--- reset.sh
+++ reset.sh
@@ -5,11 +5,13 @@ export KUBECONFIG=`pwd`/kubeconfig
 
 kubectl delete -f ./deployments/nextcloud-server.yaml
 kubectl delete -f ./deployments/nextcloud-db.yaml
+kubectl delete -f ./deployments/photoprism-server.yaml
 
 read -p "Would you like to delete ALL of the persistent state? (users, calendars, uploaded images) <y/N> " prompt
 if [[ $prompt =~ [yY](es)* ]]
 then
-    echo kubectl delete -f ./deployments/nextcloud-shared-pvc.y
+    echo kubectl delete -f ./deployments/nextcloud-shared-pvc.yaml
+    echo kubectl delete -f ./deployments/photoprism-shared-pvc.yaml
 fi
 
 kubectl delete -f ./deployments/cluster-ingress.yaml
--- start.sh
+++ start.sh
@@ -7,3 +7,5 @@ kubectl apply -f ./deployments/cluster-ingress.yaml
 kubectl apply -f ./deployments/nextcloud-shared-pvc.yaml
 kubectl apply -f ./deployments/nextcloud-db.yaml
 kubectl apply -f ./deployments/nextcloud-server.yaml
+kubectl apply -f ./deployments/photoprism-shared-pvc.yaml
+kubectl apply -f ./deployments/photoprism-server.yaml
